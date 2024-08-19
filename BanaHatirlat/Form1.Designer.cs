36f18b844a5b9cca5e96aa704d7bede1fd277a93
mertcankomurcu <mertcankomurcu@outlook.com.tr>
2024-08-19 12:52:04 +0300

    * Başlangıç ve Bitiş saatleri eklendi. * Geri Sayım uygulama ismine eklendi. * Geçen süre eklendi.

--- BanaHatirlat/Form1.Designer.cs
+++ BanaHatirlat/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.BtnBaslat = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.LblVersiyon = new System.Windows.Forms.Label();
             this.TxtSure = new System.Windows.Forms.TextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.BtnMuzikSec = new System.Windows.Forms.Button();
@@ -43,7 +44,17 @@
             this.label2 = new System.Windows.Forms.Label();
             this.BtnGenislet = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.GecenSure = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BasZamani = new System.Windows.Forms.Label();
+            this.BitZamani = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,7 +64,7 @@
             this.BtnBaslat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.BtnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.BtnBaslat.ForeColor = System.Drawing.Color.White;
-            this.BtnBaslat.Location = new System.Drawing.Point(218, 101);
+            this.BtnBaslat.Location = new System.Drawing.Point(250, 99);
             this.BtnBaslat.Name = "BtnBaslat";
             this.BtnBaslat.Size = new System.Drawing.Size(95, 26);
             this.BtnBaslat.TabIndex = 0;
@@ -66,7 +77,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(205, 44);
+            this.label1.Location = new System.Drawing.Point(243, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 18);
             this.label1.TabIndex = 1;
@@ -75,14 +86,23 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.LblVersiyon);
             this.panel1.Location = new System.Drawing.Point(-1, -1);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(170, 170);
+            this.panel1.Size = new System.Drawing.Size(181, 200);
             this.panel1.TabIndex = 2;
+            // 
+            // LblVersiyon
+            // 
+            this.LblVersiyon.AutoSize = true;
+            this.LblVersiyon.Location = new System.Drawing.Point(32, 111);
+            this.LblVersiyon.Name = "LblVersiyon";
+            this.LblVersiyon.Size = new System.Drawing.Size(0, 18);
+            this.LblVersiyon.TabIndex = 0;
             // 
             // TxtSure
             // 
-            this.TxtSure.Location = new System.Drawing.Point(258, 41);
+            this.TxtSure.Location = new System.Drawing.Point(296, 39);
             this.TxtSure.Name = "TxtSure";
             this.TxtSure.Size = new System.Drawing.Size(49, 24);
             this.TxtSure.TabIndex = 3;
@@ -94,7 +114,7 @@
             // 
             // BtnMuzikSec
             // 
-            this.BtnMuzikSec.Location = new System.Drawing.Point(172, 137);
+            this.BtnMuzikSec.Location = new System.Drawing.Point(186, 125);
             this.BtnMuzikSec.Name = "BtnMuzikSec";
             this.BtnMuzikSec.Size = new System.Drawing.Size(35, 32);
             this.BtnMuzikSec.TabIndex = 4;
@@ -109,11 +129,10 @@
             // TxtDosyaYolu
             // 
             this.TxtDosyaYolu.Enabled = false;
-            this.TxtDosyaYolu.Location = new System.Drawing.Point(213, 141);
+            this.TxtDosyaYolu.Location = new System.Drawing.Point(227, 130);
             this.TxtDosyaYolu.Name = "TxtDosyaYolu";
-            this.TxtDosyaYolu.Size = new System.Drawing.Size(100, 24);
+            this.TxtDosyaYolu.Size = new System.Drawing.Size(141, 24);
             this.TxtDosyaYolu.TabIndex = 0;
-            this.TxtDosyaYolu.TextChanged += new System.EventHandler(this.TxtDosyaYolu_TextChanged);
             // 
             // TimerPanel
             // 
@@ -122,7 +141,7 @@
             // 
             // TxtTekrar
             // 
-            this.TxtTekrar.Location = new System.Drawing.Point(258, 71);
+            this.TxtTekrar.Location = new System.Drawing.Point(296, 69);
             this.TxtTekrar.Name = "TxtTekrar";
             this.TxtTekrar.Size = new System.Drawing.Size(49, 24);
             this.TxtTekrar.TabIndex = 6;
@@ -133,7 +152,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(191, 74);
+            this.label2.Location = new System.Drawing.Point(229, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 18);
             this.label2.TabIndex = 7;
@@ -158,28 +177,102 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Checked = true;
             this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(178, 77);
+            this.checkBox1.Location = new System.Drawing.Point(216, 75);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(15, 14);
             this.checkBox1.TabIndex = 9;
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // GecenSure
+            // 
+            this.GecenSure.AutoSize = true;
+            this.GecenSure.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.GecenSure.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.GecenSure.Location = new System.Drawing.Point(281, 7);
+            this.GecenSure.Name = "GecenSure";
+            this.GecenSure.Size = new System.Drawing.Size(0, 24);
+            this.GecenSure.TabIndex = 10;
+            // 
             // axWindowsMediaPlayer1
             // 
             this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 189);
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 221);
             this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
             this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(142, 23);
             this.axWindowsMediaPlayer1.TabIndex = 5;
             this.axWindowsMediaPlayer1.Visible = false;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(180, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Başlangıç Zamanı :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(216, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 18);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Bitiş Zamanı :";
+            // 
+            // BasZamani
+            // 
+            this.BasZamani.AutoSize = true;
+            this.BasZamani.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BasZamani.Location = new System.Drawing.Point(311, 159);
+            this.BasZamani.Name = "BasZamani";
+            this.BasZamani.Size = new System.Drawing.Size(0, 18);
+            this.BasZamani.TabIndex = 13;
+            // 
+            // BitZamani
+            // 
+            this.BitZamani.AutoSize = true;
+            this.BitZamani.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BitZamani.Location = new System.Drawing.Point(311, 177);
+            this.BitZamani.Name = "BitZamani";
+            this.BitZamani.Size = new System.Drawing.Size(0, 18);
+            this.BitZamani.TabIndex = 14;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(180, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 18);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Geçen Süre :";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(134)))), ((int)(((byte)(133)))));
-            this.ClientSize = new System.Drawing.Size(165, 135);
+            this.ClientSize = new System.Drawing.Size(180, 199);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BitZamani);
+            this.Controls.Add(this.BasZamani);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.GecenSure);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.BtnGenislet);
             this.Controls.Add(this.label2);
@@ -196,11 +289,11 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Bana Hatırlat";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -223,6 +316,16 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button BtnGenislet;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label LblVersiyon;
+        private System.Windows.Forms.Label GecenSure;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label BasZamani;
+        private System.Windows.Forms.Label BitZamani;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- BanaHatirlat/Form1.cs
+++ BanaHatirlat/Form1.cs
@@ -6,24 +6,82 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Media;
+using System.Net.Http;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Reflection;
 
 namespace BanaHatirlat
 {
     public partial class Form1 : Form
     {
+        private DateTime dt;
+        private int secondsElapsed;
+        private int secondsRemaining; // Geri sayım süresini tutacak sayaç
+        private string formName; // Formun adını saklayacak değişken
+        bool isStopped = false;
+        bool isPanelOn = false;
         public Form1()
         {
             InitializeComponent();
-        }
-        bool isStopped = false;
-        bool isPanelOn = false;
+            formName = this.Text; // Başlangıçta formun adı
 
+            // Sayaç başlangıç değeri
+            secondsElapsed = 0;
+
+            // Label'ın başlangıç metni
+            GecenSure.Text = TimeSpan.FromSeconds(secondsElapsed).ToString(@"hh\:mm\:ss");
+        }
+
+
+        private void BtnBaslat_Click(object sender, EventArgs e)
+        {
+
+            dt = DateTime.Now;
+            DateTime dtBitis = dt.AddMinutes(Convert.ToDouble(TxtSure.Text));
+
+            if (timer1.Enabled)
+            {
+                // Eğer timer zaten çalışıyorsa, durdur
+                timer1.Stop();
+                TimerPanel.Stop();
+                BtnBaslat.Text = "Başlat"; // Butonun metnini "Başlat" olarak güncelle
+                isStopped = true;
+                timer2.Stop();
+                timer3.Stop();
+
+            }
+            else
+            {
+                // Eğer timer çalışmıyorsa, başlat
+
+                timer1.Interval = int.Parse(TxtSure.Text) * 60 * 1000;
+                timer1.Start();
+                isStopped = false;
+                BtnBaslat.Text = "Durdur";
+                BasZamani.Text = dt.ToString("HH:mm:ss");
+                BitZamani.Text = dtBitis.ToString("HH:mm:ss");
+                secondsElapsed = 0;
+                GecenSure.Text = TimeSpan.FromSeconds(secondsElapsed).ToString(@"hh\:mm\:ss");
+                timer2.Start();
+                timer3.Start();
+                secondsRemaining = int.Parse(TxtSure.Text)*60;
+                UpdateTitle();
+
+            }
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
+            // Geçen sürenin sayacını artır
+            secondsElapsed++;
+
+            // Geçen süreyi label'a yaz
+            GecenSure.Text = TimeSpan.FromSeconds(secondsElapsed).ToString(@"hh\:mm\:ss");
             if (!isStopped)
             {
                 string musicPath = TxtDosyaYolu.Text;
@@ -55,27 +113,6 @@ namespace BanaHatirlat
             }
 
         }
-        private void BtnBaslat_Click(object sender, EventArgs e)
-        {
-            if (timer1.Enabled)
-            {
-                // Eğer timer zaten çalışıyorsa, durdur
-                timer1.Stop();
-                TimerPanel.Stop();
-                BtnBaslat.Text = "Başlat"; // Butonun metnini "Başlat" olarak güncelle
-                isStopped = true;
-            }
-            else
-            {
-                // Eğer timer çalışmıyorsa, başlat
-                
-                timer1.Interval = int.Parse(TxtSure.Text)*60  * 1000;
-                timer1.Start();
-                isStopped = false;
-                BtnBaslat.Text = "Durdur";
-            }
-        }
-
         private void TimerPanel_Tick(object sender, EventArgs e)
         {
             if (isPanelOn)
@@ -104,30 +141,51 @@ namespace BanaHatirlat
 
         private void BtnGenislet_Click(object sender, EventArgs e)
         {
-            if (this.Size.Width < 320 || this.Size.Height < 178) // Küçük boyut kontrolü
+            if (this.Size.Width < 405 || this.Size.Height < 242) // Küçük boyut kontrolü
             {
                 // Eğer formun boyutu küçükse, daha büyük bir boyuta genişlet
-                this.Size = new Size(332, 208); // Örnek olarak genişletilmiş bir boyut
+                this.Size = new Size(405, 242); // Örnek olarak genişletilmiş bir boyut
                 BtnGenislet.Text = "Küçült";
+ 
             }
+
+
             else
             {
                 // Eğer formun boyutu büyükse, daha küçük bir boyuta küçült
-                this.Size = new Size(185, 178); // Örnek olarak küçültülmüş bir boyut
+                this.Size = new Size(200, 242); // Örnek olarak küçültülmüş bir boyut
                 BtnGenislet.Text = "Genişlet";
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void timer2_Tick(object sender, EventArgs e)
         {
-            string previousValue = Properties.Settings.Default.PreviousValue;
-            TxtDosyaYolu.Text = previousValue;
+            // Geçen sürenin sayacını artır
+            secondsElapsed++;
+
+            // Geçen süreyi label'a yaz
+            GecenSure.Text = TimeSpan.FromSeconds(secondsElapsed).ToString(@"hh\:mm\:ss");
         }
 
-        private void TxtDosyaYolu_TextChanged(object sender, EventArgs e)
+        private void timer3_Tick(object sender, EventArgs e)
         {
-            Properties.Settings.Default.PreviousValue = TxtDosyaYolu.Text;
-            Properties.Settings.Default.Save();
+            // Eğer geri sayım süresi bitmemişse
+            if (secondsRemaining > 0)
+            {
+                secondsRemaining--; // Süreyi bir saniye azalt
+                UpdateTitle(); // Başlığı güncelle
+            }
+            else
+            {
+                // Geri sayım tamamlandığında timer'ı durdur
+                timer3.Stop();
+            }
+        }
+        private void UpdateTitle()
+        {
+            // Başlığı güncelle
+            this.Text = $"{formName} [{TimeSpan.FromSeconds(secondsRemaining).ToString(@"mm\:ss")}]";
+
         }
     }
 }
