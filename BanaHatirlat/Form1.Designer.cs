f70219532262856e32df967e79b12e8c835733d2
mertcankomurcu <mertcankomurcu@outlook.com.tr>
2025-08-22 23:23:45 +0300

    test

--- BanaHatirlat/Form1.Designer.cs
+++ BanaHatirlat/Form1.Designer.cs
@@ -287,6 +287,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
